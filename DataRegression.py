4faf16a5b2fdc63a8ad390d5b113aba9bc8bfa91
Matthieu HERNANDEZ <matthieu.hernandez1994@gmail.com>
2019-10-31 09:28:37 +0100

    Refacto in progress

--- DataRegression.py
+++ DataRegression.py
@@ -10,41 +10,53 @@ from Data import *
 class Regression:
 
     @staticmethod
-    def LoadSet(folder, folder2 = ""):
-        data = Regression.LoadData(folder, folder2)
-        labels = Regression.LoadLabels(folder, folder2)
-        values =(data, labels)
-        return values
-    
-    @staticmethod
-    def LoadData(folder, folder2):
-        set = []
-        path = "dataset\\" + folder + "\\inputs\\*.png"
-        for filename in glob.glob(path):
-            img = mpimg.imread(filename)
-            img = Regression.__ConvertToGrayscale(img)
-            #plt.imshow(img, cmap='gray')
-            #plt.show()
-            data = img.reshape(20, 20, 1)
-            #if folder != "train":###########
-            set.append(data)
-        if folder2 != "":
-            path = "dataset\\" + folder2 + "\\*.png"
+    def LoadSet(folders):
+        inputs = []
+        labels = []
+        for folder in folders:
+  
+            if folder == "train":
+                #labels = labels +
+                LoadJson(folder)
+                
+            path = "dataset\\" + folder + "\\inputs\\*.png"
             for filename in glob.glob(path):
                 img = mpimg.imread(filename)
                 img = Regression.__ConvertToGrayscale(img)
+
+                #plt.figure('window title')
+                plt.imshow(img, cmap='gray')##
+
                 data = img.reshape(20, 20, 1)
-                set.append(data)
-        return np.asarray(set)
+                inputs.append(data)
+                
+                if folder != "train":
+                    filename = filename.split('\\')[-1]
+                    name = filename.split('_')[0]
+                    value = filename.split('_')[1].split('.')[0]
+                    if "fake" in folder:
+                        label = float(value)/1000.0
+                    else:
+                        label = float(value) / SpellCooldowns[name]
+                    labels.append(label)
+
+                plt.show()
+
+        return (np.asarray(inputs), np.asarray(labels))
     
     @staticmethod
-    def LoadLabels(folder, folder2):
-        labels = LoadJson(folder)       
+    def LoadJson(folder):
+        print("a")
+        labels = LoadJson("dataset\\" + folder + "\\labels.json")       
         for key in labels :
             name = key.split('_')[0]
             labels[key] = labels[key] / SpellCooldowns[name]
-
-        values2 = []  
+        sorted_labels = sorted(labels.items(), key=operator.itemgetter(0))
+        values = [x[1] for x in sorted_labels]
+        print("b")
+        return values
+    
+        '''values2 = []  
         if folder2 != "":
             path = "dataset\\" + folder2 + "\\*.png"
             for filename in glob.glob(path):
@@ -52,31 +64,35 @@ class Regression:
                 value = float(label)/1000.0
                 values2.append(value)
         sorted_labels = sorted(labels.items(), key=operator.itemgetter(0))
-        values = [x[1] for x in sorted_labels]
+        
         values = values2 + values
         #if folder == "train":###########
         #    values = values2
-        return np.asarray(values)
+        return np.asarray(values)'''
     
     def __ConvertToGrayscale(image):
         return np.dot(image[...,:3], [0.2989, 0.5870, 0.1140])
 
     @staticmethod
     def __ImageTest():
-        img = mpimg.imread(file)
-        print(img)
-        print(img.shape)
+        img = mpimg.imread("train_fake//inputs//black125.png")
+        #print(img)
+        #print(img.shape)
         plt.imshow(img) # 20 *20 * 3
         imgFlat = img.flatten(order='C')
-        print(imgFlat) # 1 * 1200
-        plt.show()
+        #print(imgFlat) # 1 * 1200
+        #plt.show()
 
+showData
+for earch
+image[0]
+label as title
 
 if __name__ == "__main__":
 
-    print("Start")
-    
+    print("Start")  
     #ImageTest()
-    trainSet = Regression.LoadSet("train")
+    trainSet = Regression.LoadSet(["train", "train_fake", "test_hard"])
+    showData(trainSet)
     print(trainSet)
     print("End")
